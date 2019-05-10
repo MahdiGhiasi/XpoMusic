060e6330252056ea7ffe6376c93cef3102855be3
Mahdi Ghiasi <mghiasi75@gmail.com>
2019-05-10 16:46:44 +0430

    initLightTheme separate operations so all that can succeed, succeed.

--- ModernSpotifyUWP/InjectedAssets/initLightTheme.js
+++ ModernSpotifyUWP/InjectedAssets/initLightTheme.js
@@ -3,9 +3,20 @@
 var body = document.getElementsByTagName('body')[0];
 body.setAttribute('data-scriptinjection-lighttheme', 1);
 
+errors = "";
+
 // Inject css
-var css = '{{CSSBASE64CONTENT}}';
-var style = document.createElement('style');
-document.getElementsByTagName('head')[0].appendChild(style);
-style.type = 'text/css';
-style.appendChild(document.createTextNode(atob(css)));
+try {
+    var css = '{{CSSBASE64CONTENT}}';
+    var style = document.createElement('style');
+    document.getElementsByTagName('head')[0].appendChild(style);
+    style.type = 'text/css';
+    style.appendChild(document.createTextNode(atob(css)));
+}
+catch (ex) {
+    errors += "injectLightCssFailed,";
+}
+
+
+if (errors.length > 0)
+    throw errors;
