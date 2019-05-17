c74ded55bd5356113b52d66f73d3a3afdf791670
Mahdi Ghiasi <mghiasi75@gmail.com>
2019-05-17 21:34:20 +0430

    Fixed class name change

--- ModernSpotifyUWP/InjectedAssets/initScript.js
+++ ModernSpotifyUWP/InjectedAssets/initScript.js
@@ -5,7 +5,12 @@ function onResize() {
          *  I couldn't fix the width of the new track list for Edge (Works fine in Chrome but not in Edge),
          *  so I use a javascript workaround for that.
          */
-        document.querySelectorAll(".main-view-container__content")[0].style.width = (window.innerWidth - document.querySelectorAll(".Root__nav-bar")[0].offsetWidth) + "px";
+        var contentDiv = document.querySelectorAll(".main-view-container__content");
+        if (contentDiv.length === 0) {
+            contentDiv = document.querySelectorAll(".main-view-container__scroll-node");
+        }
+
+        contentDiv[0].style.width = (window.innerWidth - document.querySelectorAll(".Root__nav-bar")[0].offsetWidth) + "px";
     }
     catch (ex) {
         console.log("resize event failed");
