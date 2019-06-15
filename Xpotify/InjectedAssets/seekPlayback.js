18d2f2ebf3a92fd7db6caeb162f0e3585cd986cf
Mahdi Ghiasi <mghiasi75@gmail.com>
2019-06-15 20:40:26 +0430

    Create seekPlayback.js

---
+++ Xpotify/InjectedAssets/seekPlayback.js
@@ -0,0 +1,22 @@
+﻿
+var percentage = Number.parseFloat('{{percentage}}');
+
+var rect = progressBar.getBoundingClientRect();
+var x = rect.left + rect.width * percentage;
+var y = rect.top + rect.height / 2;
+
+var mouseDownEvent = document.createEvent('MouseEvents');
+mouseDownEvent.initMouseEvent(
+    'mousedown', true, true, window, 0,
+    0, 0, x, y, false, false,
+    false, false, 0, null
+);
+document.elementFromPoint(x, y).dispatchEvent(mouseDownEvent);
+
+var mouseUpEvent = document.createEvent('MouseEvents');
+mouseUpEvent.initMouseEvent(
+    'mouseup', true, true, window, 0,
+    0, 0, x, y, false, false,
+    false, false, 0, null
+);
+document.elementFromPoint(x, y).dispatchEvent(mouseUpEvent);
