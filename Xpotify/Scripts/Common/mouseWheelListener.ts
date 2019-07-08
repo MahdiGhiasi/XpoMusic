ac692c4ed141d0e353b9de934912933b84edd5ac
Mahdi Ghiasi <mghiasi75@gmail.com>
2019-07-08 23:41:42 +0430

    Fixed volume bar not present at first moment causing script injection to fail

--- Xpotify/Scripts/Common/mouseWheelListener.ts
+++ Xpotify/Scripts/Common/mouseWheelListener.ts
@@ -13,6 +13,13 @@ namespace XpotifyScript.Common.MouseWheelListener {
 
     function setVolumeBarListener() {
         var volumeBar = document.querySelector(".Root__top-container .Root__now-playing-bar .now-playing-bar__right__inner .volume-bar > div");
+
+        if (volumeBar === null) {
+            // Volume bar not present yet. Will try again later.
+            setTimeout(setVolumeBarListener, 1000);
+            return;
+        }
+
         volumeBar.addEventListener("mousewheel", (e) => { volumeMouseWheelHandler(e) });
     }
 
